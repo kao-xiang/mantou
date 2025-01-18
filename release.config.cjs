6f0ff40a8df55af15fd70872b71d6f9224692802
ppenter <jarupak.sri@gmail.com>
2025-01-19 03:05:54 +0700

    fix: sematic release

--- release.config.cjs
+++ release.config.cjs
@@ -1,52 +1,46 @@
 /** @type {import('semantic-release').GlobalConfig} */
 module.exports = {
-  extends: "semantic-release-monorepo",
-  tagFormat: "v${version}",
-  branches: [
-    "master",
-    {
-      name: "develop",
-      prerelease: "canary",
-      channel: "canary"
-    }
-  ],
-  plugins: [
-    ["@semantic-release/commit-analyzer", {
-      preset: "angular",
-      releaseRules: [
-        {type: "feat", release: "minor"},
-        {type: "fix", release: "patch"},
-        {type: "docs", release: "patch"},
-        {type: "style", scope: "ui", release: "patch"},
-        {type: "refactor", release: "patch"},
-        {type: "perf", release: "patch"},
-        {type: "test", release: "patch"}
-      ]
-    }],
+  "plugins": [
+    "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     ["@semantic-release/npm", {
-      pkgRoot: `packages/${process.env.PACKAGE_NAME}`,
-      npmPublish: true,
-      tarballDir: "dist"
+      "pkgRoot": "packages/${process.env.PACKAGE_NAME}"
     }],
     ["@semantic-release/git", {
-      assets: [
-        `packages/${process.env.PACKAGE_NAME}/package.json`,
-        `packages/${process.env.PACKAGE_NAME}/CHANGELOG.md`
+      "assets": [
+        "packages/${process.env.PACKAGE_NAME}/package.json",
+        "packages/${process.env.PACKAGE_NAME}/CHANGELOG.md"
       ],
-      message: "chore(release): v${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      "message": "chore(release): ${process.env.PACKAGE_NAME} v${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
     ["@semantic-release/github", {
-      assets: [{
-        path: "dist/*.tgz",
-        label: "npm package"
-      }],
-      successComment: false,
-      failTitle: false,
-      releasedLabels: false,
-      addReleases: "bottom",
-      releaseNameTemplate: "Release <%= nextRelease.version %> <%= branch.channel ? '(' + branch.channel + ')' : '' %>"
+      "assets": [
+        {
+          "path": "packages/${process.env.PACKAGE_NAME}/dist/**",
+          "label": "${process.env.PACKAGE_NAME} distribution"
+        }
+      ]
     }]
-  ]
+  ],
+  "branches": ["main"],
+  "monorepo": {
+    "packageFiles": [
+      "packages/*/package.json"
+    ],
+    "plugins": [
+      ["@semantic-release/commit-analyzer", {
+        "preset": "angular",
+        "releaseRules": [
+          {"type": "feat", "release": "minor"},
+          {"type": "fix", "release": "patch"},
+          {"type": "docs", "release": "patch"},
+          {"type": "style", "scope": "ui", "release": "patch"},
+          {"type": "refactor", "release": "patch"},
+          {"type": "perf", "release": "patch"},
+          {"type": "test", "release": "patch"}
+        ]
+      }]
+    ]
+  }
 }
