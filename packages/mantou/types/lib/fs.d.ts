fc7aa9997d5ef33707ba8b22ab3a6e9afb4e2539
ppenter <jarupak.sri@gmail.com>
2025-01-24 01:30:10 +0700

    Update fs.d.ts

--- packages/mantou/types/lib/fs.d.ts
+++ packages/mantou/types/lib/fs.d.ts
@@ -1,3 +1,3 @@
-import type { ServerOptions } from '@/types/server';
+import type { ServerOptions } from "@/types/server";
 export declare const loadConfig: (configPath?: string, _options?: ServerOptions) => Promise<ServerOptions<any>>;
-export declare function writeRecursive(path: string, content: string): Promise<boolean>;
+export declare function writeRecursive(filePath: string, content: string): Promise<boolean>;
